fb685b0d16dbd651466031f4f516152680cf8f37
Manuqlly <selvaperumal20005@gmail.com>
2024-12-12 10:56:15 +0530

    tailwind

--- Frontend/src/pages/home.jsx
+++ Frontend/src/pages/home.jsx
@@ -1,1 +1,14 @@
+
 import React from "react";
+import AnimatedGridPattern from '../components/ui/animated-grid-pattern'
+
+function Home() {
+    return (
+        <>
+            <AnimatedGridPattern />
+            
+        </>
+    );
+}
+
+export default Home;
